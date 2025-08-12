4317ff67e8f26dbe6b1642f2bbcf04888cf4d2e5
Jose Encarnacion <Jose.Encarnacion@dpworld.com.do>
2025-08-12 10:23:46 -0400

    first commit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+const W_WIDTH = 600;
+const W_HEIGHT = 800;
+
+
 function setup() {
  // put setup code here
- createCanvas(400, 400);
+ var canvas = createCanvas(W_WIDTH, W_HEIGHT);
+ canvas.parent('cnv')
+// createCanvas(W_WIDTH, W_HEIGHT);
+ 
+ // Create a p5.Color object using a RGBA color string.
+  let c3 = color('rgba(0, 0, 255, 1)');
+  fill(c3);
+  square(10, 55, 35);
+
+
+
 }
 
 function draw() {
   // put drawing code here
+  
 }
